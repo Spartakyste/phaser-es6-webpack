43019ec4a05c82f90324fcacebaa78b43dce9582
Axel <axeletcana@hotmail.fr>
2020-05-05 23:31:49 +0200

    Moved post be out of class

--- src/scenes/user/CreateUser.js
+++ src/scenes/user/CreateUser.js
@@ -1,8 +1,13 @@
 /* globals __DEV__ */
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 import axios from 'axios';
 
 import configuration from '../../../configuration';
+
+const postUser = async (callback) => {
+    await axios.post(`${configuration.BE_URL}/user`, { user: { username: this.inputValue } });
+    callback();
+};
 
 export default class CreateUser extends Phaser.Scene {
     constructor() {
@@ -11,24 +16,19 @@ export default class CreateUser extends Phaser.Scene {
         this.inputValue = '';
     }
 
-    postUser = async () => {
-        await axios.post(configuration.BE_URL + "/user", { user: { username: this.inputValue } });
-        this.scene.start('StarterScene');
-    };
 
-    init() {
+    // init() {
 
-    }
+    // }
 
-    preload() {
+    // preload() {
 
-    }
+    // }
 
     create() {
-
         this.add.image(0, 0, 'sky');
 
-        const backButton = this.add.text(0, 0, "Go back", { fontFamily: 'Arial', fontSize: 20, color: '#000000' }).setInteractive();
+        const backButton = this.add.text(0, 0, 'Go back', { fontFamily: 'Arial', fontSize: 20, color: '#000000' }).setInteractive();
 
         backButton.on('pointerdown', () => this.scene.start('MenuScene'));
 
@@ -37,27 +37,24 @@ export default class CreateUser extends Phaser.Scene {
         const interactiveContainer = this.add.rectangle(400, 300, 400, 50, 0xACACAC, 1).setInteractive();
 
 
-        interactiveContainer.on("pointerdown", () => {
+        interactiveContainer.on('pointerdown', () => {
             const input = this.add.rectangle(400, 300, 300, 25, 0xDDDDDD, 1).setInteractive();
             const inputText = this.add.text(300, 290, this.inputValue);
 
             const button = this.add.rectangle(500, 400, 100, 25, 0xDDDDDD, 1).setInteractive();
             const buttonText = this.add.text(470, 390, 'Envoyer');
 
-            button.on('pointerdown', this.postUser);
+            button.on('pointerdown', postUser(() => this.scene.start('StarterScene')));
 
             this.input.keyboard.on('keydown', (event) => {
                 if ((event.key === 'Backspace') && (this.inputValue.length >= 1)) {
-                    this.inputValue = this.inputValue.slice(0, -1)
+                    this.inputValue = this.inputValue.slice(0, -1);
                 } else {
                     if (event.key.length > 1) return;
-                    this.inputValue = this.inputValue + event.key;
+                    this.inputValue += event.key;
                 }
                 inputText.setText(this.inputValue);
             });
         });
-
-        
     }
-
 }
